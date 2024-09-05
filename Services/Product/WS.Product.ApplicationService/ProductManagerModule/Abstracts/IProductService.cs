29976432c14e2c3cc530e726841b65b112095a2c
Lê Văn Minh <44087123+vanminhdev@users.noreply.github.com>
2024-09-05 09:17:23 +0700

    Update IProductService.cs

--- Services/Product/WS.Product.ApplicationService/ProductManagerModule/Abstracts/IProductService.cs
+++ Services/Product/WS.Product.ApplicationService/ProductManagerModule/Abstracts/IProductService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WS.Product.ApplicationService.ProductManagerModule.Abstracts
 {
-    internal interface IProductService
+    public interface IProductService
     {
     }
 }
