383aefadd784d7782f2387c9938d40393a30bceb
Lê Văn Minh <44087123+vanminhdev@users.noreply.github.com>
2024-09-30 16:46:49 +0700

    .

--- Services/Product/WS.Product.ApplicationService/ProductManagerModule/Implements/ProductService.cs
+++ Services/Product/WS.Product.ApplicationService/ProductManagerModule/Implements/ProductService.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using WS.Product.ApplicationService.Common;
 using WS.Product.ApplicationService.ProductManagerModule.Abstracts;
 using WS.Product.Infrastructure;
@@ -15,7 +15,12 @@ namespace WS.Product.ApplicationService.ProductManagerModule.Implements
     {
         private IUserInforService _userInforService;
 
-        public ProductService(ILogger<ProductService> logger, ProductDbContext dbContext, IUserInforService userInforService) : base(logger, dbContext)
+        public ProductService(
+            ILogger<ProductService> logger,
+            ProductDbContext dbContext,
+            IUserInforService userInforService
+        )
+            : base(logger, dbContext)
         {
             _userInforService = userInforService;
         }
---
+++ WS.WebAPI/Controllers/Product/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WS.Product.ApplicationService.ProductManagerModule.Abstracts;
+
+namespace WS.WebAPI.Controllers.Product
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        //Thêm
+
+        //Sửa
+
+        //xoá
+
+        //lấy danh sách (có phân trang)
+
+        //lấy theo id
+    }
+}
